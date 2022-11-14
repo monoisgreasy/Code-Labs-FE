1e1757b10b135ba05b0457fd8128991b7d9bb25d
selina <torsoxs@gmail.com>
2022-11-14 13:58:22 -0600

    finished seeing lifecycle hooks in action
    I had some trouble with this one working

--- cmp-databinding-start/src/app/server-elements/server-elements.component.ts
+++ cmp-databinding-start/src/app/server-elements/server-elements.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-server-elements',
@@ -6,13 +6,46 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-elements.component.css'],
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementsComponent implements OnInit {
+export class ServerElementsComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constuctor called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!')
+  }
+
+  ngAfterContentInit(): void {
+      console.log('ngAfterContentInit called!')
+  }
+
+  ngAfterContentChecked(): void {
+      console.log('ngAfterContentChecked called!')
+  }
+
+  ngAfterViewInit(): void {
+      console.log('ngAfterVeiwInit checked!')
+  }
+
+  ngAfterViewChecked(): void {
+      console.log('ngAfterViewChecked called!')
+  }
+
+  ngOnDestroy(): void {
+      console.log('ngOnDestroy called!')
   }
 
 }
