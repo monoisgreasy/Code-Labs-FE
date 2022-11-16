918d8a7ecbe026b8fe693718c43ba9f638fa9602
selina <torsoxs@gmail.com>
2022-11-16 13:33:49 -0600

    finished working on the shoppinglistcomponent

--- my-games-app/src/app/splatoon-details/splatoon-details.component.ts
+++ my-games-app/src/app/splatoon-details/splatoon-details.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./splatoon-details.component.css']
 })
 export class SplatoonDetailsComponent implements OnInit {
+  details = [];
 
   constructor() { }
 
