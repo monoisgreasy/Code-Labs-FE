abdfa7c7b36c7506151156a8414c11286691978e
selina <torsoxs@gmail.com>
2022-11-18 12:49:43 -0600

    finished creating and outputting the shopping list
    for project

--- my-games-app/src/app/splatoon-details/splatoon-details.component.ts
+++ my-games-app/src/app/splatoon-details/splatoon-details.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Details } from '../shared/details.model';
+
 @Component({
   selector: 'app-splatoon-details',
   templateUrl: './splatoon-details.component.html',
   styleUrls: ['./splatoon-details.component.css']
 })
 export class SplatoonDetailsComponent implements OnInit {
-  details = [];
+  detail: Details[] = [
+    new Details('Inklings have a bridge above their noses that connects their eyes!', 100000000),
+    new Details('Octolings have no bridges above their noses!', 10000000),
+  ];
 
   constructor() { }
 
